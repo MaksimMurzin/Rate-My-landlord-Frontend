05e5e51cebcf52e208cee7ecf525b79b8259681d
Maksim Murzin <maks_murzin@yahoo.com>
2026-01-09 18:58:46 +0300

    added ids for rentals

--- src/app/rentals.service.ts
+++ src/app/rentals.service.ts
@@ -9,10 +9,11 @@ export class RentalService {
   getRentals()
   {
     return [
-      new Rental('St. Peters Court', 500),
-      new Rental('10 Kittiwake Mews', 625),
-      new Rental('Miverna House', 650),
-      new Rental('10 Cartwright Way', 500)
+      new Rental(1,'St. Peters Court', 500),
+      new Rental(2,'10 Kittiwake Mews', 625),
+      new Rental(3,'Miverna House', 650),
+      new Rental(4,'10 Cartwright Way', 500),
+      new Rental(5,'Littlebrook Ave', 600)
   ]
   }
 }
