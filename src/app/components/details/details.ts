12d805c04ba1998c19dcf7479a822a61eabfb1e8
Maksim Murzin <maks_murzin@yahoo.com>
2026-01-19 23:06:21 +0300

    added star rating system and refactored css

--- src/app/components/details/details.ts
+++ src/app/components/details/details.ts
@@ -2,24 +2,17 @@ import { Component, inject, input, computed } from '@angular/core'; // Added com
 import { RentalService } from '../../services/rentals.service';
 import { CurrencyPipe, NgOptimizedImage } from '@angular/common';
 import { Rental } from '../../models/rental.model';
+import { StarRating } from '../star-rating/star-rating';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.html',
   styleUrl: './details.css',
-  imports: [NgOptimizedImage, CurrencyPipe],
+  imports: [NgOptimizedImage, CurrencyPipe, StarRating],
 })
 export class Details {
   private readonly rentalService = inject(RentalService);
   id = input.required<string>(); // Use required for safety
-
-  // rental: Rental = {
-  //   id: Number(this.id),
-  //   address: 'test address',
-  //   price: 444.0,
-  //   rating: 2,
-  //   pictureUrl: 'images/default_house.jpg',
-  // };
 
   // 2. Use a computed signal to reactively fetch the data
   // This automatically re-runs if the URL ID changes!
--- src/app/components/rental-card/rental-card.component.ts
+++ src/app/components/rental-card/rental-card.component.ts
@@ -1,13 +1,14 @@
 import { Component, inject, input } from '@angular/core';
 import { Rental } from '../../models/rental.model';
 import { CurrencyPipe, NgOptimizedImage } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { StarRating } from '../star-rating/star-rating';
 
 @Component({
   selector: 'app-rental-card',
   templateUrl: './rental-card.component.html',
   styleUrl: './rental-card.component.css',
-  imports: [CurrencyPipe, NgOptimizedImage, RouterLink],
+  imports: [CurrencyPipe, NgOptimizedImage, RouterLink, StarRating],
 })
 export class RentalCardComponent {
   rental = input.required<Rental>();
